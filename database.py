78d59c76c28421d5d3ce3abae9b89449e7bc2988
Niranjana <niranjana2397av@gmail.com>
2026-04-10 13:49:54 +0530

    Added three more functions

--- database.py
+++ database.py
@@ -41,12 +41,71 @@ def view_task():
 
     if total_tasks ==[]:
         print("No tasks have been added")
+
     else:
         print(total_tasks)
 
-def update_task(id,title):
+def update_task(task_id):
     db_connect , cursor = connect_tasks_db()
-    command4 = """UPDATE tasks SET title = ? WHERE id = ?"""
-    cursor.execute(command4,(id,title))
-    db_connect.commit()
+    
+    cursor.execute("SELECT id FROM Tasks WHERE id=?",(task_id,))
+    results = cursor.fetchone()
+    if results:
+        new_title=input("Enter the new title: ")
+        command4 = "UPDATE tasks SET title = ? WHERE id = ?"
+        cursor.execute(command4,(new_title,task_id))
+        db_connect.commit()
+        print("\nTask is updated succesfullly")
+    else:
+        print("\nThe task doesn't exist")
+        
+    
+
+
+def mark_task_completed(task_id):
+    db_connect , cursor = connect_tasks_db()
+    cursor.execute("SELECT id FROM Tasks WHERE id=?",(task_id,))
+    results = cursor.fetchone()
+    if results:
+        cursor.execute("SELECT status FROM Tasks WHERE id=?", (task_id,))
+        results1 = cursor.fetchone()
+        if results1[0] == 'Pending':
+            command5 = "UPDATE tasks SET status = 'Completed' WHERE id = ?"
+            cursor.execute(command5,(task_id,))
+            db_connect.commit()
+            print("\nStatus is updated to 'completed'")
+        else:
+            print("\nThis task is already completed and not pending anymore")
+    else:
+        print("\nThe task doesn't exist")
+
+    
+def delete_task(task_id):
+    db_connect , cursor = connect_tasks_db()
+    cursor.execute("SELECT id FROM Tasks WHERE id=?",(task_id,))
+    results = cursor.fetchone()
+    if results:
+        cursor.execute("DELETE FROM Tasks WHERE id=?", (task_id,))
+        db_connect.commit()
+        print("\nTask is deleted successfully")
+    else:
+        print("\nThe task doesn't exist")
+
+
+def filter_tasks(status):
+    db_connect , cursor = connect_tasks_db()    
+    command6 = "SELECT * FROM Tasks WHERE status=?"
+    
+    filter_tasks = cursor.execute(command6,(status,)).fetchall()
+    print(filter_tasks)
+    
+    
+
+
+
+    
+
+
+
+    
     
--- main.py
+++ main.py
@@ -17,12 +17,24 @@ n = 0
 while n!=7: 
       welcome_page()
       n = int(input("Please choose the number from the above options"))
+            
       if n not in range(1,8):
           print("\nYou chose the wrong number! Please choose the right option")
       elif n==1:
-        database.add_task(title=input("Enter the task name "))
+        database.add_task(title=input("Enter the task name ").capitalize())
       elif n==2:
         database.view_task()
       elif(n==3):
-        database.update_task(id=int(input("Please enter the task id to change")),title =input("Please enter the new title"))
+        database.update_task(task_id=int(input("Please enter the task id to change the title")))
+      elif(n==4):
+        database.mark_task_completed(task_id=int(input("Please enter the task id to change the status")))
+      elif(n==5):
+        database.delete_task(task_id=int(input("Please enter the task id to delete the task")))
+      elif(n==6):
+        database.filter_tasks(status = input("Please enter the status to filter the tasks").capitalize())
+      else:
+          print("Goodbye!")
+
       
+         
+          
